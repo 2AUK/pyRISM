Fixed issue with unit conversion happening twice

pyrism/Potentials/potential_routines.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.special import erf
 
 
-def Lennard_Jones(r, params, lam, beta):
+def Lennard_Jones(r, params, lam):
     """
     Computes the Lennard-Jones potential with epsilon and sigma parameters
 
@@ -25,10 +25,10 @@ def Lennard_Jones(r, params, lam, beta):
         The result of the LJ computation
     """
     eps, sig = params
-    return beta * 4.0 * eps * ((sig / r) ** 12 - (sig / r) ** 6) * lam
+    return 4.0 * eps * ((sig / r) ** 12 - (sig / r) ** 6) * lam
 
 
-def Lennard_Jones_AB(r, C6, C12, lam, beta):
+def Lennard_Jones_AB(r, C6, C12, lam):
     """
     Computes the Lennard-Jones potential with C6 and C12 parameters
 
@@ -51,10 +51,10 @@ def Lennard_Jones_AB(r, C6, C12, lam, beta):
         The result of the LJ computation
     """
 
-    return beta * ((C12 / r ** 12) - (C6 / r ** 6)) * lam
+    return ((C12 / r ** 12) - (C6 / r ** 6)) * lam
 
 
-def hard_spheres(r, sigma, lam, beta):
+def hard_spheres(r, sigma, lam):
     """
     Computes the Lennard-Jones potential with C6 and C12 parameters
 
@@ -77,10 +77,10 @@ def hard_spheres(r, sigma, lam, beta):
         The result of the LJ computation
     """
 
-    return beta * np.where((r >= sigma), 0, np.inf) * lam
+    return np.where((r >= sigma), 0, np.inf) * lam
 
 
-def coulomb(r, q1, q2, lam, beta, charge_coeff):
+def coulomb(r, q1, q2, lam, charge_coeff):
     """
     Computes the Coulomb potential
 
@@ -102,10 +102,10 @@ def coulomb(r, q1, q2, lam, beta, charge_coeff):
     result: float
         The result of the LJ computation
     """
-    return lam * beta * charge_coeff * q1 * q2 / r
+    return lam * charge_coeff * q1 * q2 / r
 
 
-def coulomb_lr_r(r, q1, q2, damping, rscreen, lam, beta, charge_coeff):
+def coulomb_lr_r(r, q1, q2, damping, rscreen, lam, charge_coeff):
     """
     Computes the Ng renorm potential
 
@@ -129,10 +129,10 @@ def coulomb_lr_r(r, q1, q2, damping, rscreen, lam, beta, charge_coeff):
     result: float
         The result of the LJ computation
     """
-    return lam * beta * charge_coeff * q1 * q2 * erf(damping * r / rscreen) / r
+    return lam * charge_coeff * q1 * q2 * erf(damping * r / rscreen) / r
 
 
-def coulomb_lr_k(k, q1, q2, damping, lam, beta, charge_coeff):
+def coulomb_lr_k(k, q1, q2, damping, lam, charge_coeff):
     """
     Computes the Ng renorm potential
 
@@ -158,7 +158,6 @@ def coulomb_lr_k(k, q1, q2, damping, lam, beta, charge_coeff):
     """
     return (
         lam
-        * beta
         * 4
         * np.pi
         * q1

pyrism/rism_ctrl.py
@@ -143,16 +143,16 @@ class RismController:
                         qi = iat.params[-1]
                         qj = jat.params[-1]
                         if iat == jat:
-                            dat.u[:, i, j] = sr_pot(dat.grid.ri, i_sr_params, lam, dat.B) \
-                                + cou(dat.grid.ri, qi, qj, lam, dat.B, dat.amph)
+                            dat.u[:, i, j] = sr_pot(dat.grid.ri, i_sr_params, lam) \
+                                + cou(dat.grid.ri, qi, qj, lam, dat.amph)
                         else:
-                            mixed = mix(iat.params, jat.params)
-                            dat.u[:, i, j] = sr_pot(dat.grid.ri, mixed, lam, dat.B) \
-                                + cou(dat.grid.ri, qi, qj, lam, dat.B, dat.amph)
+                            mixed = mix(i_sr_params, j_sr_params)
+                            dat.u[:, i, j] = sr_pot(dat.grid.ri, mixed, lam) \
+                                + cou(dat.grid.ri, qi, qj, lam, dat.amph)
                         j += 1
                 i += 1
 
-    def build_renorm(self, dat, damping = 1.0, lam = 1):
+    def build_renorm(self, dat, damping=1.0, lam=1):
         erfr, _ = Potentials.Potential("erfr").get_potential()
         erfk, _ = Potentials.Potential("erfk").get_potential()
         i = 0
@@ -163,8 +163,8 @@ class RismController:
                     for jat in jsp.atom_sites:
                         qi = iat.params[-1]
                         qj = jat.params[-1]
-                        dat.ur_lr[:, i, j] = erfr(dat.grid.ri, qi, qj, damping, 1.0, lam, dat.B, dat.amph)
-                        dat.uk_lr[:, i, j] = erfk(dat.grid.ki, qi, qj, damping, lam, dat.B, dat.amph)
+                        dat.ur_lr[:, i, j] = erfr(dat.grid.ri, qi, qj, damping, 1.0, lam, dat.amph)
+                        dat.uk_lr[:, i, j] = erfk(dat.grid.ki, qi, qj, damping, lam, dat.amph)
                         j += 1
                 i += 1
 
@@ -184,15 +184,16 @@ class RismController:
             self.build_Ur(dat, lam)
             self.build_renorm(dat, 1.0, lam)
             dat.u_sr = dat.u - dat.ur_lr
+            fr = np.exp(-(dat.B * dat.u_sr)) - 1.0
             if j == 1:
-                dat.c = np.exp(-1 * (dat.u_sr)) - 1.0
+                dat.c = np.zeros_like(fr)
             else:
                 pass
             self.solver.solve(IE, clos, lam)
                         
         dat.c -= dat.B * dat.ur_lr
         dat.t += dat.B * dat.ur_lr
-        gr = 1 + dat.c + dat.t
+        gr = 1.0 + dat.c + dat.t
         plt.plot(dat.grid.ri, gr[:, 0, 0])
         plt.show()
 