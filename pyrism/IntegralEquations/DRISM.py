1b9f8a52acf1c653ad6f3ffe49215d919265dd56
2auk <ahmada1uk@googlemail.com>
2021-11-26 16:14:15 +0000

    started DRISM D matrix implementation

--- pyrism/IntegralEquations/DRISM.py
+++ pyrism/IntegralEquations/DRISM.py
@@ -10,7 +10,10 @@ class DRISM(object):
 
     data_vv: RISM_Obj
     data_uv: RISM_Obj = None
-    h_c0: np.ndarray = None
+    h_k: np.ndarray = None
+    h_c0: float = None
+    y: float = None
+    diel: float = None
 
     def compute_vv(self):
         pass
@@ -18,14 +21,25 @@ class DRISM(object):
     def compute_uv(self):
         pass
 
-    def calculate_y(self):
+    def calculate_DRISM_params(self):
         total_density = 0
         dm, _ = Util.dipole_moment(self.data_vv)
         for isp in self.data_vv.species:
             total_density += isp.dens
         dmdensity = total_density * dm * dm
+        self.y = 4.0 * np.pi * self.data_vv.B * dmdensity / 9.0
+        self.h_c0 = ((self.diel - 1.0) / self.y) * total_density
 
-        return 4.0 * np.pi * self.data_vv.B * dmdensity / 9.0
+    def D_matrix(self):
+        for k in self.data_vv.grid.ki:
+            for isp in self.data_vv.species:
+                for iat in isp:
+                    k_coord = iat.coords * k
+                    if k_coord[0] == 0:
+                        pass
+
+    def __post_init__(self):
+        self.calculate_DRISM_params()
 
 
 def vv_impl():
