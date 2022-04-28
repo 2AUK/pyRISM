bfc4eaedde9891db0a42886ecf2c9d612e773e32
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2022-04-28 17:18:24 +0100

    Unpacking all the site lists to use in PairTable

--- libpyrism/mol/pairtable.py
+++ libpyrism/mol/pairtable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass, field
-from itertools import product
+from itertools import product, chain
 
 @dataclass
 class PairTable:
@@ -21,13 +21,29 @@ class PairTable:
     vpairs: list = field(init=False, default_factory=list)
     uvpairs: list = field(init=False, default_factory=list)
 
-    def generate_table(self):
+    def generate_tables(self):
         self.species.sort()
-        solvent_species = [x for x in self.species if x.speciestype == SpeciesKind.Solvent]
-        solute_species = [x for x in self.species if x.speciestype == SpeciesKind.Solute]
+        all_species = chain(*[x.site for x in self.species])
+        solvent_species = chain(*[x.site for x in self.species if x.speciestype == SpeciesKind.solute])
+        solute_species = chain(*[x.site for x in self.species if x.speciestype == SpeciesKind.Solute])
         self.upairs = self._pairs_in_list(solvent_species)
         self.vpairs = self._pairs_in_list(solute_species)
-        self.uvpairs = self._pairs_in_list(self.species)
+        self.uvpairs = self._pairs_in_list(all_species)
+
+    def iter_solute_pairs(self):
+        return iter(self.upairs)
+
+    def iter_solvent_pairs(self):
+        return iter(self.vpairs)
+
+    def iter_all_pairs(self):
+        return iter(self.uvpairs)
 
     def _pairs_in_list(self, input_list):
         return list(product(self.params, repeat=2))
+
+    def _filter_species(self, input_list, filter):
+        for x in input_list:
+            if filter(x):
+                return true
+            return false
--- libpyrism/system/RISM.py
+++ libpyrism/system/RISM.py
@@ -32,3 +32,4 @@ class RISMProblem:
         """
         Perform checks, generate PairTable, initialise functions.
         """
+        pass
