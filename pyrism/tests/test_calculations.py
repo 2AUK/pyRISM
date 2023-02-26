bf8fc75c072fb77b926dc4ff4601351889946c17
2auk <ahmada1uk@googlemail.com>
2023-02-26 21:52:03 +0000

    Groundwork for test calcs

--- pyrism/tests/test_calculations.py
+++ pyrism/tests/test_calculations.py
@@ -7,12 +7,12 @@ class TestRISMJobs(unittest.TestCase):
     def test_2_propanol(self):
         mol = RismController("inputs/2_propanol.toml")
         mol.initialise_controller()
-        #mol.do_rism()
+        mol.do_rism()
 
-        test_case = np.genfromtxt('outputs/2_propanol/2_propanol.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.data_uv.grid.npts, mol.data_uv.ns1, mol.data_uv.ns2))
+        test_case = np.genfromtxt('outputs/2_propanol/2_propanol.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
 
-        print(test_case)
-        print(test_case.shape)
+        np.allclose(test_case, mol.uv.g, 1e-7, 1e-7)
+
 
 unittest.main()        
 
