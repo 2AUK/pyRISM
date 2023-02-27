3b8fbba9bd90c3cfcc945ce09a3472b7f0a485f9
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-02-27 16:04:15 +0000

    fixing CI workflow (relative paths?)

--- pyrism/tests/test_calculations.py
+++ pyrism/tests/test_calculations.py
@@ -10,11 +10,11 @@ class TestRISMJobs(unittest.TestCase):
         warnings.simplefilter('ignore')
 
     def test_2_propanol(self):
-        mol = RismController("inputs/2_propanol.toml")
+        mol = RismController("./inputs/2_propanol.toml")
         mol.initialise_controller()
         mol.do_rism(verbose=self.verbosity)
 
-        test_case = np.genfromtxt('outputs/2_propanol/2_propanol.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
+        test_case = np.genfromtxt('./outputs/2_propanol/2_propanol.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
 
         np.allclose(test_case, mol.uv.g, 1e-7, 1e-7)
 
