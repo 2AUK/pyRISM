a8c2dfeea47dc7c7611b5bf0eba2e857b75548ce
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-02-27 15:47:35 +0000

    Fixed tests

--- pyrism/tests/test_calculations.py
+++ pyrism/tests/test_calculations.py
@@ -59,27 +59,27 @@ class TestRISMJobs(unittest.TestCase):
         mol.initialise_controller()
         mol.do_rism(verbose=self.verbosity)
 
-        test_case = np.genfromtxt('outputs/cSPCE/cSPCE.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
+        test_case = np.genfromtxt('outputs/cSPCE/cSPCE.gvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        np.allclose(test_case, mol.uv.g, 1e-7, 1e-7)
+        np.allclose(test_case, mol.vv.g, 1e-7, 1e-7)
 
     def test_cSPCE_NaCl(self):
         mol = RismController("inputs/cSPCE_NaCl.toml")
         mol.initialise_controller()
         mol.do_rism(verbose=self.verbosity)
 
-        test_case = np.genfromtxt('outputs/cSPCE_NaCl/cSPCE_NaCl.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
+        test_case = np.genfromtxt('outputs/cSPCE_NaCl/cSPCE_NaCl.gvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        np.allclose(test_case, mol.uv.g, 1e-7, 1e-7)
+        np.allclose(test_case, mol.vv.g, 1e-7, 1e-7)
 
     def test_HR1982N(self):
         mol = RismController("inputs/HR1982N.toml")
         mol.initialise_controller()
         mol.do_rism(verbose=self.verbosity)
 
-        test_case = np.genfromtxt('outputs/HR1982N/HR1982N.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
+        test_case = np.genfromtxt('outputs/HR1982N/HR1982N.gvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        np.allclose(test_case, mol.uv.g, 1e-7, 1e-7)
+        np.allclose(test_case, mol.vv.g, 1e-7, 1e-7)
 if __name__ == "__main__":
     unittest.main()        
 
