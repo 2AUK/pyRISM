04f5c9b5bda9089174607990c6e5aaa2fd0b4592
2auk <ahmada1uk@googlemail.com>
2023-03-01 12:34:28 +0000

    changed to assert_allclose with atol 1e-4

--- pyrism/tests/test_calculations.py
+++ pyrism/tests/test_calculations.py
@@ -1,6 +1,6 @@
 from pyrism.rism_ctrl import *
 import unittest
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_allclose
 import warnings
 from pathlib import Path
 
@@ -8,6 +8,8 @@ class TestRISMJobs(unittest.TestCase):
 
     def setUp(self):
         self.verbosity = False 
+        self.rtol = 0.0
+        self.atol = 1e-4
         warnings.simplefilter('ignore')
 
     def test_2_propanol(self):
@@ -17,7 +19,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('./outputs/2_propanol/2_propanol.guv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
 
-        assert_array_almost_equal(test_case, mol.uv.g)
+        assert_allclose(test_case, mol.uv.g, self.rtol, self.atol)
 
     def test_argon_uv(self):
         mol = RismController("inputs/argon_uv.toml")
@@ -26,7 +28,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/argon_uv/argon_uv.tuv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
 
-        assert_array_almost_equal(test_case, mol.uv.t)
+        assert_allclose(test_case, mol.uv.t, self.rtol, self.atol)
 
     def test_chg_org_solu(self):
         mol = RismController("inputs/chg_org_solu.toml")
@@ -35,7 +37,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/chg_org_solu/chg_org_solu.cuv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.uv.grid.npts, mol.uv.ns1, mol.uv.ns2))
 
-        assert_array_almost_equal(test_case, mol.uv.c)
+        assert_allclose(test_case, mol.uv.c, self.rtol, self.atol)
 
     def test_chloro(self):
         mol = RismController("inputs/chloro.toml")
@@ -44,7 +46,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/chloro/chloro_SFED.duv', delimiter=',', skip_header=2, usecols=1)
 
-        assert_array_almost_equal(test_case, mol.SFED['HNC'])
+        assert_allclose(test_case, mol.SFED['HNC'], self.rtol, self.atol)
 
     def test_chloro_cg(self):
         mol = RismController("inputs/chloro_cg.toml")
@@ -53,7 +55,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/chloro_cg/chloro_cg_SFED.duv', delimiter=',', skip_header=2, usecols=2)
 
-        assert_array_almost_equal(test_case, mol.SFED['KH'])
+        assert_allclose(test_case, mol.SFED['KH'], self.rtol, self.atol)
 
     def test_nhexylbenzene(self):
         mol = RismController("inputs/nhexylbenzene.toml")
@@ -62,7 +64,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/nhexylbenzene/nhexylbenzene_SFED.duv', delimiter=',', skip_header=2, usecols=3)
 
-        assert_array_almost_equal(test_case, mol.SFED['GF'])
+        assert_allclose(test_case, mol.SFED['GF'], self.rtol, self.atol)
     
     def test_cSPCE(self):
         mol = RismController("inputs/cSPCE.toml")
@@ -71,7 +73,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/cSPCE/cSPCE.gvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        assert_array_almost_equal(test_case, mol.vv.g)
+        assert_allclose(test_case, mol.vv.g, self.rtol, self.atol)
 
     def test_cSPCE_NaCl(self):
         mol = RismController("inputs/cSPCE_NaCl.toml")
@@ -80,7 +82,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/cSPCE_NaCl/cSPCE_NaCl.tvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        assert_array_almost_equal(test_case, mol.vv.t)
+        assert_allclose(test_case, mol.vv.t, self.rtol, self.atol)
 
     def test_HR1982_Br2_IV(self):
         mol = RismController("inputs/HR1982_Br2_IV.toml")
@@ -89,7 +91,7 @@ class TestRISMJobs(unittest.TestCase):
 
         test_case = np.genfromtxt('outputs/HR1982_Br2_IV/HR1982_Br2_IV.cvv', delimiter=',', skip_header=2)[:, 1:].reshape((mol.vv.grid.npts, mol.vv.ns1, mol.vv.ns2))
 
-        assert_array_almost_equal(test_case, mol.vv.c)
+        assert_allclose(test_case, mol.vv.c, self.rtol, self.atol)
 
 if __name__ == "__main__":
     unittest.main()        
