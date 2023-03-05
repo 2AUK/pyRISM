e711b28bb69d620ca4921b34cf0d77f83675821c
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-03-05 23:22:54 +0000

    Gillan; rework basis function (only calc once)

--- pyrism/Solvers/Gillan.py
+++ pyrism/Solvers/Gillan.py
@@ -2,14 +2,16 @@
 
 from pyrism.Core import RISM_Obj
 import numpy as np
+from numpy.testing import assert_allclose
 from scipy.optimize import newton_krylov
 from .Solver_object import *
+import matplotlib.pyplot as plt
 
 @dataclass
 class Gillan(SolverObject):
     nbasis: int = field(default=4)
 
-    def solve(self, RISM, Closure, lam):
+    def solve(self, RISM, Closure, lam, verbose=False):
         npts = self.data_vv.grid.npts
         ns1 = self.data_vv.ns1
         ns2 = self.data_vv.ns2
@@ -21,6 +23,9 @@ class Gillan(SolverObject):
         fine = t.copy()
         fine[:node_index[-1], ...] = 0
 
+        print(fine)
+        print(nodes.shape)
+
 
         P = np.zeros((npts, ns1, ns2, self.nbasis+1), dtype=np.float64)
         Q = np.zeros_like(P)
@@ -29,30 +34,46 @@ class Gillan(SolverObject):
         A = np.zeros((self.nbasis), dtype=np.float64)
 
         P[..., 0] = 1
-        for idx in range(0, npts):
-            if nodes[0] <= t[idx, ...] <= nodes[1]:
-                P[idx, 1] = (nodes[1] - t[idx, ...]) / (nodes[1])
+        for i, m, n in np.ndindex(npts, ns1, ns2):
+            if nodes[0, m, n] <= t[i, m, n] <= nodes[1, m, n]:
+                P[i, m, n, 1] = (nodes[1, m, n] - t[i, m, n]) / (nodes[1, m, n])
 
-        for a in range(2, nbasis+1):
-            for idx in range(0, npts):
-                if nodes[a-2] <= t[idx] <= nodes[a-1]:
-                    P[idx, a] = (t[idx] - nodes[a-2])/(nodes[a-1] - nodes[a-2])
-                elif nodes[a-1] <= t[idx] <= nodes[a]:
-                    P[idx, a] = (nodes[a] - t[idx])/(nodes[a] - nodes[a-1])
+        for a in range(2, self.nbasis+1):
+            for i, m, n in np.ndindex(npts, ns1, ns2):
+                if nodes[a-2, m, n] <= t[i, m, n] <= nodes[a, m, n]:
+                    P[i, m, n, a] = (t[i, m, n] - nodes[a-2, m, n])/(nodes[a-1, m, n] - nodes[a-2, m, n])
+                elif nodes[a-1] <= t[i, m, n] <= nodes[a, m, n]:
+                    P[i, m, n, a] = (nodes[a, m, n] - t[i, m, n])/(nodes[a, m, n] - nodes[a-1, m, n])
+
+        for a in range(0, self.nbasis+1):
+            for m, n in np.ndindex(ns1, ns2):
+                print(P[..., m, n, a])
+                plt.plot(r, P[..., m, n, a])
+        plt.show()
 
         P_proper = P[..., 1:].copy()
-        P_a = P_proper.copy()
-        P_b = P_proper.copy()
+        P_skip_zero = P_proper.copy()
+        Pa = P_proper.copy()
+        Pb = P_proper.copy()
 
-        for a in range(nbasis):
-            for b in range(nbasis):
+        for a in range(self.nbasis):
+            for b in range(self.nbasis):
                 R[a,b] = (Pa[..., a] * Pb[..., b]).sum()
 
         B = np.linalg.inv(R)
 
         for i in range(npts):
-            for a in range(nbasis):
+            for a in range(self.nbasis):
                 Q[i,a] = (B[a,:] * P_skip_zero[i,:]).sum()
+
+        kron = np.zeros_like(R)
+
+        for a in range(self.nbasis):
+            for b in range(self.nbasis):
+                kron[a,b] = (Q[...,a] * P_skip_zero[...,b]).sum()
+
+        identity = np.identity((self.nbasis, self.nbasis), dtype=np.float64)
+        assert_allclose(kron, nbasis, atol=1e-4, rtol=1e-4)
 
         for a in range(nbasis):
             A[a] = (Q[..., a] * t).sum()
