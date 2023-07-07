55fe3fd3ebf6a8a5c253e0233c04feda0d8abb63
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-07-07 13:05:51 +0100

    united atom bad; all atom, pretty good

--- pyrism/tests/test_thermo.py
+++ pyrism/tests/test_thermo.py
@@ -1,19 +1,23 @@
 from pyrism.rism_ctrl import *
 import matplotlib.pyplot as plt
 
-mol = RismController("../data/cSPCE_XRISM_methane_UA.toml")
+mol = RismController("../data/cSPCE_XRISM_methane.toml")
 
 mol.initialise_controller()
 
 mol.do_rism(verbose=True)
 
-plt.plot(mol.vv.grid.ri, mol.uv.c[:, 0, 0])
-plt.show()
+#plt.plot(mol.vv.grid.ri, mol.uv.c[:, 0, 0])
+#plt.show()
 
 print("isothermal compressibility:", (mol.isothermal_compressibility(mol.vv)))
 
 print("pressure:", mol.pressure())
 
 print("PMV:", mol.partial_molar_volume())
+
+_, PMV = mol.partial_molar_volume()
+
+print("PMV (cm^3 / mol):", PMV / 1e36 * 6.022E23)
 
 print("PC+ - SFE:", mol.pc_plus())
