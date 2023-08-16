dc5b5d7086333e1fd8f758ef1be911c8a8c32e74
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-08-16 16:09:25 +0100

    pulling training inputs from full datasets

--- pyrism/cnn_data/scaling_param_gen.py
+++ pyrism/cnn_data/scaling_param_gen.py
@@ -3,11 +3,11 @@ from pathlib import Path
 import pandas as pd
 
 files = Path('./model').resolve()
-
+df_data_1 = pd.read_csv("multi-T_gf_dataset_water-both_chloroform_carbontet.csv")
+df_data_2 = pd.read_csv("free_energy_neutral_ionised_dataset_gf.csv")
+df_data = pd.concat([df_data_1, df_data_2], ignore_index=True).drop_duplicates()
 for f in files.iterdir():
     print(f.stem)
     df = pd.read_csv(str(f) + '/' + f.stem + '/' + f.stem + '_train_plot.csv')
-    y = df['y'].to_numpy()
-    y_pred = df['y_pred'].to_numpy()
-    print(y.mean(), y.std())
-    print(y_pred.mean(), y_pred.std())
+    df_train = pd.merge(df_data.sort_values(by='Temp'), df.sort_values(by='Temp'), how='inner', on=['Mol', 'Temp'])
+    print(df_train.sort_values(by='Mol'))
