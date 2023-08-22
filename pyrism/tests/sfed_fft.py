540cafa22394be2c986b217ef5b6362291150477
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-08-22 13:15:27 +0100

    simple script for plotting rfft with freqs

--- pyrism/tests/sfed_fft.py
+++ pyrism/tests/sfed_fft.py
@@ -8,5 +8,11 @@ mol.initialise_controller()
 
 mol.do_rism(verbose=True)
 
-
 print(mol.SFED['KH'])
+
+fft_sfed = rfft(mol.SFED['KH'])
+freqs = rfftfreq(mol.SFED['KH'].shape[0], mol.vv.grid.d_r)
+
+plt.plot(mol.vv.grid.ri, mol.SFED['KH'])
+plt.plot(freqs, np.abs(fft_sfed))
+plt.show()
