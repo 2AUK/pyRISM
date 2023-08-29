039b579f84ce9cfa5358aaf47e8d5bd7ba3a75ea
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-08-29 14:53:50 +0100

    setting up inputs for xrism integral equation

--- src/transforms.rs
+++ src/transforms.rs
@@ -1,11 +1,35 @@
+use ndarray::Array1;
 use rustdct::TransformType4;
 use std::sync::Arc;
-use ndarray::Array1;
 
 type FFTPlan = Arc<dyn TransformType4<f64>>;
 
-pub fn dfbt(prefac: f64, grid1: Array1<f64>, grid2: Array1<f64>, func: Array1<f64>, plan: &FFTPlan) -> Array1<f64> {
+pub fn fourier_bessel_transform(
+    prefac: f64,
+    grid1: &Array1<f64>,
+    grid2: &Array1<f64>,
+    func: &Array1<f64>,
+    plan: &FFTPlan,
+) -> Array1<f64> {
     let mut buffer = (func * grid1).to_vec();
     plan.process_dst4(&mut buffer);
     prefac * Array1::from_vec(buffer) / grid2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ndarray::{Array, Axis};
+    use ndarray_rand::rand_distr::Uniform;
+    use ndarray_rand::RandomExt;
+
+    #[test]
+    fn random_test() {
+        let array = Array::random((16, 2, 2), Uniform::new(0., 10.));
+        println!("{array}");
+        array.lanes(Axis(0)).into_iter().for_each(|a| {
+            println!("{a}");
+            println!("{}", a.len());
+        });
+    }
 }
--- src/xrism.rs
+++ src/xrism.rs
@@ -1,14 +1,50 @@
-use ndarray::{Array1, Array3};
+use crate::transforms::fourier_bessel_transform;
+use ndarray::{Array, Array1, Array2, Array3, Axis, Zip};
+use rustdct::DctPlanner;
+use std::f64::consts::PI;
 
 fn xrism_vv_equation(
     ns: usize,
     npts: usize,
-    ck: Array3<f64>,
+    r: Array1<f64>,
+    k: Array1<f64>,
+    dr: f64,
+    dk: f64,
+    cr: Array3<f64>,
     wk: Array3<f64>,
     p: Array1<f64>,
     B: f64,
     uk_lr: Array3<f64>,
     ur_lr: Array3<f64>,
 ) -> Array3<f64> {
-    todo!()
+    // Setting up prefactors for Fourier-Bessel transforms
+    let rtok = 2.0 * PI * dr;
+    let ktor = dk / (4.0 * PI * PI);
+
+    // Starting FFT Plan
+    let plan = DctPlanner::new().plan_dst4(npts);
+
+    // Setting up arrays used in calculating XRISM equation
+    let identity: Array2<f64> = Array::eye(ns);
+    let mut ck = Array::zeros(cr.raw_dim());
+    let mut hk = Array::zeros(cr.raw_dim());
+    let mut tr = Array::zeros(cr.raw_dim());
+
+    // Transforming c(r) -> c(k)
+    Zip::from(cr.lanes(Axis(0)))
+        .and(ck.lanes_mut(Axis(0)))
+        .par_for_each(|cr_lane, mut ck_lane| {
+            ck_lane.assign(&fourier_bessel_transform(
+                rtok,
+                &r,
+                &k,
+                &cr_lane.to_owned(),
+                &plan,
+            ));
+        });
+    
+    // Adding long-range component back in
+    ck = ck - B * uk_lr;
+
+    ck
 }
