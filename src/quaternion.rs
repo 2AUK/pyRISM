9673b3dafc0b79a73214c7d62ff39b9a4390d229
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-10-02 23:39:59 +0100

    Adding quaternion newtype (wrapped ndarray)

--- src/quaternion.rs
+++ src/quaternion.rs
@@ -1,1 +1,21 @@
 use ndarray::{Array, Array1};
+
+#[derive(Clone, Debug)]
+pub struct Quaternion(Array1<f64>);
+
+impl Quaternion {
+    pub fn new(input_arr: [f64; 4]) -> Self {
+        Quaternion(Array::from_iter(input_arr.into_iter()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_quat() {
+        let quat = Quaternion::new([1.0, 2.0, 3.0, 4.0]);
+        println!("{:?}", quat);
+    }
+}
