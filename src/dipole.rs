331f6d9ce4c66c11df88be8f5bb5b689ce97415e
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-10-03 20:54:38 +0100

    function for computing total charge (needs test)

--- src/dipole.rs
+++ src/dipole.rs
@@ -1,1 +1,17 @@
+use crate::data::Species;
 use crate::quaternion::Quaternion;
+use ndarray::{Array, Array1};
+
+pub fn total_charge(species: Vec<Species>) -> f64 {
+    species.iter().fold(0.0, |acc, x| {
+        acc + x
+            .atom_sites
+            .iter()
+            .fold(0.0, |acc_inner, y| acc_inner + y.params.last().unwrap())
+    })
+}
+
+pub fn centre_of_charge(species: Vec<Species>) -> Array1<f64> {
+    let out = Array::zeros(3);
+    out
+}
