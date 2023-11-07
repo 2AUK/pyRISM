e5e3452e0f9dd5fc9bb3adc0bfd199cfb1ac6773
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-11-07 16:56:02 +0000

    Working on python results
    Need to include grid with the final output, otherwise can't plot anything...

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,13 @@ extern crate blas_src;
 pub use crate::drivers::rism::RISMDriver;
 pub use crate::io::writer::RISMWriter;
 pub use crate::thermodynamics::thermo::TDDriver;
+use data::solution::PyCorrelations;
+use data::solution::PyInteractions;
+use data::solution::PySolution;
+use data::solution::PySolvedData;
 use data::solution::Solutions;
 use drivers::rism::{Compress, Verbosity};
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use std::path::PathBuf;
 use thermodynamics::thermo::Thermodynamics;
@@ -22,7 +27,72 @@ pub mod thermodynamics;
 #[pyo3(name = "Calculator")]
 pub struct PyCalculator {
     pub name: String,
+    verbosity: Verbosity,
+    compress: Compress,
     driver: RISMDriver,
+}
+
+#[pymethods]
+impl PyCalculator {
+    #[new]
+    pub fn new(fname: String, verbosity: String, compress: bool) -> PyResult<Self> {
+        let path = PathBuf::from(&fname);
+        let driver = RISMDriver::from_toml(&path);
+        let name = driver.name.clone();
+        let verbosity = match verbosity.as_str() {
+            "quiet" => Verbosity::Quiet,
+            "verbose" => Verbosity::Verbose,
+            "loud" => Verbosity::VeryVerbose,
+            _ => return Err(PyTypeError::new_err("Invalid verbosity flag")),
+        };
+        let compress = match compress {
+            true => Compress::Compress,
+            false => Compress::NoCompress,
+        };
+        Ok(PyCalculator {
+            name,
+            verbosity,
+            compress,
+            driver,
+        })
+    }
+
+    pub fn execute<'py>(&'py mut self, py: Python<'py>) -> PyResult<PySolution> {
+        let solution = self.driver.execute(&self.verbosity, &self.compress);
+        let wv = self.driver.solvent.borrow().wk.clone();
+        let wu = {
+            match &self.driver.solute {
+                Some(v) => Some(v.borrow().wk.clone()),
+                None => None,
+            }
+        };
+        let thermodynamics = TDDriver::new(&solution, wv, wu).execute();
+        let _ = RISMWriter::new(&self.name, &solution, &thermodynamics).write();
+        let py_corr_vv = PyCorrelations::from_correlations(solution.vv.correlations, py);
+        let py_int_vv = PyInteractions::from_interactions(solution.vv.interactions, py);
+        let py_vv = PySolvedData {
+            interactions: py_int_vv,
+            correlations: py_corr_vv,
+        };
+
+        let py_uv = match solution.uv {
+            Some(uv) => {
+                let py_corr_uv = PyCorrelations::from_correlations(uv.correlations, py);
+                let py_int_uv = PyInteractions::from_interactions(uv.interactions, py);
+                Some(PySolvedData {
+                    interactions: py_int_uv,
+                    correlations: py_corr_uv,
+                })
+            }
+            None => None,
+        };
+
+        let solution = PySolution {
+            vv: py_vv,
+            uv: py_uv,
+        };
+        Ok(solution)
+    }
 }
 
 pub struct Calculator {
