751f2c039ea171837d94bdce349513d37bb6bcb7
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2024-01-23 16:19:06 +0000

    Implemented coefficient calculations
    - Next is Jacobian
    - Seems like the order is swapped from the reference implementation

--- src/solvers/lmv.rs
+++ src/solvers/lmv.rs
@@ -36,29 +36,63 @@ impl LMV {
         prev + self.picard_damping * diff
     }
 
-    fn get_cjk(
-        &mut self,
-        problem: &mut DataRs,
-        operator: &Operator,
-        costab: Array2<f64>,
-    ) -> Array4<f64> {
-        let shape = problem.correlations.cr.dim();
+    fn get_cjk(&mut self, costab: Array2<f64>, der: Array3<f64>) -> Array4<f64> {
+        let shape = der.dim();
         let (npts, ns1, ns2) = shape;
         let mut dp: Array1<f64> = Array::zeros(3 * self.nbasis);
-        let der = (operator.closure_der)(&problem);
+        let mut cjk: Array4<f64> = Array::zeros((ns1, ns2, self.nbasis, self.nbasis));
 
         for i in 0..ns1 {
             for j in 0..ns2 {
+                println!("ns idx: {},{}", i, j);
                 for m in 1..(3 * self.nbasis) - 1 {
+                    dp[m] = 0.0;
                     for l in 0..npts {
+                        println!("point: {}, basis: {}, before: {}", l, m, dp[[m]]);
+                        println!(
+                            "der: {}, costab: {}, der * costab: {}",
+                            der[[l, i, j]],
+                            costab[[m, l]],
+                            der[[l, i, j]] * costab[[m, l]]
+                        );
                         dp[[m]] += der[[l, i, j]] * costab[[m, l]];
+                        println!("point: {}, basis: {}, after: {}", l, m, dp[[m]]);
                     }
                     dp[m] = dp[m] / npts as f64;
                 }
+                for m in 0..(3 * self.nbasis) {
+                    println!("dp[{}] = {}", m, dp[m]);
+                }
+
+                for m in 0..self.nbasis {
+                    for k in 0..self.nbasis {
+                        let kpm_m = k + m + self.nbasis;
+                        let kmm_m = (k as isize - m as isize + self.nbasis as isize) as usize;
+                        println!("m: {}, k: {}, k-m+M: {}, k+m+M: {}", m, k, kpm_m, kmm_m,);
+                        cjk[[i, j, m, k]] = dp[kmm_m] - dp[kpm_m];
+                        println!(
+                            "c_{}{}[{}][{}] = {} - {} = {}",
+                            i,
+                            j,
+                            m,
+                            k,
+                            dp[kmm_m],
+                            dp[kpm_m],
+                            cjk[[i, j, m, k]]
+                        );
+                    }
+                }
             }
         }
-        println!("{}", dp);
-        Array::zeros((ns1, ns2, self.nbasis, self.nbasis))
+        cjk
+    }
+
+    fn get_jacobian(&mut self, invwc1w: Array3<f64>) -> Array3<f64> {
+        todo!()
+    }
+
+    fn lmv_update(&mut self, der: Array3<f64>, invwc1w: Array3<f64>) {
+        self.get_cjk(self.cos_table.clone().unwrap(), der);
     }
 }
 
@@ -81,14 +115,13 @@ impl Solver for LMV {
                         (PI * (i as f64 * 2.0 + 1.0) * (j as f64 - self.nbasis as f64)
                             / npts as f64
                             / 2.0)
-                            .cos()
+                            .cos();
+                    println!("outarr[{}][{}] = {}", j, i, out_arr[[j, i]]);
                 }
             }
-            println!("{:?}", out_arr);
             Some(out_arr)
         };
 
-        self.get_cjk(problem, operator, self.cos_table.clone().unwrap());
         Ok(SolverSuccess(1, 0.1))
     }
 }
