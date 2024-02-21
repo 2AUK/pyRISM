c2b0fe28857f6d5ad278ab94a9dacff106b9baee
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2024-02-21 15:45:59 +0000

    processing diagnostic data

--- data/toml_modifier.py
+++ data/toml_modifier.py
@@ -11,16 +11,18 @@ inp_dir = Path(sys.argv[1])
 for toml_file in inp_dir.rglob("*.toml"):
     print(toml_file)
     inp_toml = toml.load(toml_file)
-    inp_toml['system']['temp'] = 300
-    inp_toml['params']['solver'] = 'ADIIS'
-    inp_toml['params']['closure'] = 'KH'
-    inp_toml['params']['picard_damping'] = 0.2
-    inp_toml['params']['depth'] = 20
-    inp_toml['system']['npts'] = 8192
-    inp_toml['system']['radius'] = 40.96
-    inp_toml['system']['lam'] = 1
-    inp_toml['params']['mdiis_damping'] = inp_toml['params']['picard_damping']
-    if "preconverged" in inp_toml['solvent']:
-        inp_toml['solvent']['preconverged'] = "/users/tjb20156/pyRISM/data/benchmark/cSPCE_Extended_RISM_Kovalenko-Hirata_300K.bin"
-    with open(toml_file, 'w') as tfile:
+    inp_toml["system"]["temp"] = 300
+    inp_toml["params"]["solver"] = "ADIIS"
+    inp_toml["params"]["closure"] = "KH"
+    inp_toml["params"]["picard_damping"] = 0.5
+    inp_toml["params"]["depth"] = 20
+    inp_toml["system"]["npts"] = 8192
+    inp_toml["system"]["radius"] = 40.96
+    inp_toml["system"]["lam"] = 1
+    inp_toml["params"]["mdiis_damping"] = inp_toml["params"]["picard_damping"]
+    if "preconverged" in inp_toml["solvent"]:
+        inp_toml["solvent"][
+            "preconverged"
+        ] = "/users/tjb20156/pyRISM/data/benchmark/cSPCE_Extended_RISM_Kovalenko-Hirata_300K.bin"
+    with open(toml_file, "w") as tfile:
         toml.dump(inp_toml, tfile, encoder=toml.TomlNumpyEncoder())
